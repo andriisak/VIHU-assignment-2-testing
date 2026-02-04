3a48f42de68e0e0f8e0f334a526c9cc51f499594
andriisak <andrib23@ru.is>
2026-02-04 16:12:51 +0000

    isHoliday tests added

--- src/__tests__/dateUtils.test.ts
+++ src/__tests__/dateUtils.test.ts
@@ -138,11 +138,31 @@ describe("Date Utils", () => {
     it("should return all holidays", async () => {
       const holidays = await getHolidays(2025);
       expect(holidays).toHaveLength(3);
-      expect(holidays[0]).toEqual(new Date(2024, 0, 1));
-      expect(holidays[1]).toEqual(new Date(2024, 11, 25));
-      expect(holidays[2]).toEqual(new Date(2024, 11, 31));
+      expect(holidays[0]).toEqual(new Date(2025, 0, 1));
+      expect(holidays[1]).toEqual(new Date(2025, 11, 25));
+      expect(holidays[2]).toEqual(new Date(2025, 11, 31));
     })
 
+
+  })
+
+  describe(isHoliday, () => {
+
+    it("should return true if a date is a holiday", async () => {
+      const holyday_1 = new Date(2024, 0, 1);
+      const holyday_2 = new Date(2024, 11, 25);
+      const holyday_3 = new Date(2024, 11, 31);
+      expect(await isHoliday(holyday_1)).toEqual(true);
+      expect(await isHoliday(holyday_2)).toEqual(true);
+      expect(await isHoliday(holyday_3)).toEqual(true);
+    })
+
+    it("should return false if a date is a not holiday", async () => {
+      const date1 = new Date(2004, 2, 4);
+      const date2 = new Date(2004, 2, 5);
+      expect(await isHoliday(date1)).toEqual(false);
+      expect(await isHoliday(date2)).toEqual(false)
+    })
 
   })
 });
