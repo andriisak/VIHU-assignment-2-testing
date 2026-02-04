bdfdcef11526fb9037fc5fb99f50a8e45e297950
andriisak <andrib23@ru.is>
2026-02-04 17:50:23 +0000

    Ts annotation for the function add

--- src/dateUtils.ts
+++ src/dateUtils.ts
@@ -1,11 +1,11 @@
 import moment from "moment";
 import { DATE_UNIT_TYPES } from "./constants";
 
-export function getCurrentYear() {
+export function getCurrentYear(): number {
   return moment().year();
 }
 
-export function add(date, amount, type = DATE_UNIT_TYPES.DAYS) {
+export function add(date: Date, amount: number, type: DATE_UNIT_TYPES = DATE_UNIT_TYPES.DAYS): Date {
   if (!(date instanceof Date) || isNaN(date.getTime())) {
     throw new Error('Invalid date provided');
   }
